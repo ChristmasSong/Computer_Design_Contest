af67484bfe17622a6fdef9aa4d1074c1337cc5bd
宋殿硕 <43170879+ChristmasSong@users.noreply.github.com>
2020-01-20 20:20:18 +0800

    第二次更新
    RGB处理完毕，已能计算平均RGB和对比RGB，下一步是拟合曲线

--- Code/Text_OpenCV/SolutionOperator.h
+++ Code/Text_OpenCV/SolutionOperator.h
@@ -1,18 +1,23 @@
 #pragma once
 #include <iostream>
+#include<cmath>
 #include <opencv.hpp>
 
 using namespace std;
 using namespace cv;
 
-struct Solution
-{
+struct Solution{
 	string path;
 	int id;
 	int RGB_average;
+	double RGB_P;
+	double mol;
 	Solution* next;
-	Solution():path(""), id(-1), next(NULL), RGB_average(-1){}
-	Solution(int i, string p):path(p), id(-1), next(NULL), RGB_average(-1){}
+	Solution()
+		:path(""), id(-1), next(NULL), RGB_average(-1), RGB_P(-1), mol(-1){}
+	Solution(int i, string p)
+		:path(p), id(-1), next(NULL), RGB_average(-1), RGB_P(-1), mol(-1){}
+	bool set_mol(double m) { this->mol = m; return true; }
 };
 
 class SolutionList {
@@ -24,14 +29,21 @@ public:
 	SolutionList(const string* path, int path_length);
 	~SolutionList();
 	Solution* operator[](int index) const;
+	int Length();
+	void showSolutions();
+	void showRGB_P();
 };
 
 class SolutionOperator {
 private:
 	SolutionList& solutions;
+protected:
 	int getRGB_average(string path);
+	double compute_RGB_P(int index);
 public:
-	SolutionOperator(SolutionList& s):solutions(s){}
+	SolutionOperator(SolutionList& s) :solutions(s) {}
 	~SolutionOperator() {}
 	int getRGB_average(int index);
+	double* compute_RGB_P();
+	bool set_mol(int index, double mol);
 };
